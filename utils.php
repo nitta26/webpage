35d2a39aee94f0b594ae4268682a620db92534f9
nitta26 <nittajirou7925@gmail.com>
2020-07-20 17:43:00 +0900

    add station_distance()

--- utils.php
+++ utils.php
@@ -12,6 +12,7 @@
 
 	function sql_close($link) {
 		mysqli_close($link);
+		echo "<p>データベースの接続を終了しました。</p>";
 	}
 
 	function sql_select($link, $query) {
@@ -35,6 +36,26 @@
 		$lat = $row['lat'];
 		return array($lon, $lat);
 	}	
+
+	function station_distance($link, $station_name1, $station_name2) {
+		// https://qiita.com/chiyoyo/items/b10bd3864f3ce5c56291
+		$station1 = station_position($link, $station_name1);
+		$station2 = station_position($link, $station_name2);
+		//echo "<p>".$station1[0]." ".$station1[1]."</p>";
+		//echo "<p>".$station2[0]." ".$station2[1]."</p>";
+		//echo deg2rad(180);
+		$radLat1 = deg2rad($station1[1]);
+		$radLon1 = deg2rad($station1[0]);
+		$radLat2 = deg2rad($station2[1]);
+		$radLon2 = deg2rad($station2[0]);
+		$r = 6378137.0;
+		$averageLat = ($radLat1-$radLat2)/2;
+		$averageLon = ($radLon1-$radLon2)/2;
+		$dist = $r * 2 * asin(sqrt(pow(sin($averageLat), 2) 
+		+ cos($radLat1) * cos($radLat2) * pow(sin($averageLon), 2)));
+		//echo $dist;
+		return $dist;
+	}
 
 ?>
 
